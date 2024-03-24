7e2bb9c46b6ddfa10572a941cc68fc6a3921f89b
makingojo <maxyugay98@mail.ru>
2024-03-25 00:39:19 +0300

    d

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      23
+      2
       <div>
         <a href='https://vitejs.dev' target='_blank'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
